f880792dd5bb57b1dfa2ca79e95408a0ae96a0a8
Jogendrapadhy72 <109838638+Jogendrapadhy72@users.noreply.github.com>
2022-07-23 19:29:41 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,37 @@
+
+
+
+op1 = 1.1 * 6
+op2 = 1.1 + 1.1 + 1.1+ 1.1 + 1.1 + 1.1
+print(op1)
+print(op2)
+print(op1 == op2)
+
+
+
+
+'''
+x1 = 45
+print(  id(45)  )
+print(id(x1))
+
+y1 = -3.55
+print(  id(-3.55)  )
+print(id(y1))
+'''
+
+
+'''
+num1 = 100.0
+
+print(100.0 == 100.0)
+print(num1 == 100.0)
+
+print(100.0 is 100.0)
+print(num1 is 100.0)
+
+
+print(id(num1))
+print(id(100.0))
+'''
+
